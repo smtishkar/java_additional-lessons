74f0113c474c0d2e9b6bfde120c1ce6a774b1403
Sergey <smtishkar@mail.ru>
2023-02-14 18:23:35 +0300

    Урок 16. Сделали метод который возврщает значение

--- ClassAndObject.java
+++ ClassAndObject.java
@@ -6,14 +6,21 @@ public static void main(String[] args) {
     Person person1 = new Person();
     person1.name = "Рома";
     person1.age = 50;
-    person1.speak();
-    person1.sayHello();
+    // person1.speak();
+    // person1.sayHello();
+    
 
     Person person2 = new Person();
     person2.name = "Вова";
     person2.age = 20;
-    person2.speak();
-    person2.sayHello();
+    // person2.speak();
+    // person2.sayHello();
+    int year1 = person1.calculationYearsToRetirement();
+    int year2 = person2.calculationYearsToRetirement();
+    
+
+    System.out.println("Первому человека до пенсии " + year1);
+    System.out.println("Второму человека до пенсии " + year2);
 
     // System.out.println( person1.name + ' ' + person1.age);
     // System.out.println( person2.name + ' ' + person2.age);
@@ -26,8 +33,12 @@ class Person{
     String name;
     int age;
 
+    int calculationYearsToRetirement(){
+        int years = 65-age;
+        return years;
+    }
+
     void speak(){
-        
         System.out.println("Меня зовут " + name + ", мне " + age + " лет");
     }
 
