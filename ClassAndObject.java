ca06ca62068d3a8048b7d4e72c31b1fd83371b54
Sergey <smtishkar@mail.ru>
2023-02-14 18:13:19 +0300

    Урок 15. Сделала метод в классе который, делает представление

--- ClassAndObject.java
+++ ClassAndObject.java
@@ -6,13 +6,17 @@ public static void main(String[] args) {
     Person person1 = new Person();
     person1.name = "Рома";
     person1.age = 50;
-    
+    person1.speak();
+    person1.sayHello();
+
     Person person2 = new Person();
     person2.name = "Вова";
     person2.age = 20;
+    person2.speak();
+    person2.sayHello();
 
-    System.out.println( person1.name + ' ' + person1.age);
-    System.out.println( person2.name + ' ' + person2.age);
+    // System.out.println( person1.name + ' ' + person1.age);
+    // System.out.println( person2.name + ' ' + person2.age);
 }
     
 
@@ -21,5 +25,16 @@ public static void main(String[] args) {
 class Person{
     String name;
     int age;
+
+    void speak(){
+        
+        System.out.println("Меня зовут " + name + ", мне " + age + " лет");
+    }
+
+    void sayHello(){
+        System.out.println("Привет!");
+    }
+
+
 
 }
