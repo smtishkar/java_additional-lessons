0a9dd8f92f8a99ca3a553391c9d6217ce587ad33
Sergey <smtishkar@mail.ru>
2023-02-14 18:40:42 +0300

    Урок 17. Сделал метод, который принимает несколько параметров и потом посдавляет их при создании объекта класса

--- ClassAndObject.java
+++ ClassAndObject.java
@@ -2,36 +2,30 @@
  * ClassAndObject
  */
 public class ClassAndObject {
-public static void main(String[] args) {
-    Person person1 = new Person();
-    person1.name = "Рома";
-    person1.age = 50;
-    // person1.speak();
-    // person1.sayHello();
-    
+    public static void main(String[] args) {
+        Person person1 = new Person();
 
-    Person person2 = new Person();
-    person2.name = "Вова";
-    person2.age = 20;
-    // person2.speak();
-    // person2.sayHello();
-    int year1 = person1.calculationYearsToRetirement();
-    int year2 = person2.calculationYearsToRetirement();
-    
 
-    System.out.println("Первому человека до пенсии " + year1);
-    System.out.println("Второму человека до пенсии " + year2);
+        person1.setNameAndAge("Роман", 20);
+        person1.speak();
 
-    // System.out.println( person1.name + ' ' + person1.age);
-    // System.out.println( person2.name + ' ' + person2.age);
-}
-    
+        Person person2 = new Person();
 
+        person2.setNameAndAge("Вова", 30);
+        person2.speak();
+    }
 }
 
 class Person{
     String name;
     int age;
+
+
+    void setNameAndAge(String username, int userage){
+        name = username;
+        age = userage;
+
+    }
 
     int calculationYearsToRetirement(){
         int years = 65-age;
